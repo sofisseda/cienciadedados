d1ffaf7f88e9984d9bcf175f4c5d14165732525c
sofisseda <145019342+sofisseda@users.noreply.github.com>
2024-10-16 12:09:19 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,8 +1,7 @@
 import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/sofisseda
-/api/main/numero-usuarios.json'
+    const url = 'https://raw.githubusercontent.com/sofisseda/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
