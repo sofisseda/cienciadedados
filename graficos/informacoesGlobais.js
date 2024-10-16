5bfb1a7840dddbf2936bbb68ce958b231938e15e
sofisseda <145019342+sofisseda@users.noreply.github.com>
2024-10-16 12:06:04 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,5 @@
-const url = 'https://raw.githubusercontent.com/victorborbasilva/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/sofisseda
+/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
